a3c0a8408bcc98cf38dbf23143f4afa5fc6b0227
LeonEidelman <leoneidelman09@gmail.com>
2025-10-03 10:46:06 +0300

    fix express route wildcard

--- frontend/server.js
+++ frontend/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(join(__dirname, 'dist')));
 
 // Handle client-side routing
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 

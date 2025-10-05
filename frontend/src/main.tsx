e924b54c5d5c13f3702198ff2e4d779ff0fc7a7d
LeonEidelman <leoneidelman09@gmail.com>
2025-10-05 16:10:57 +0300

    primer removal from main.jsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 // import './index.css';
 import App from './App.tsx';
-import '@primer/react-brand/lib/css/main.css';
-import '@primer/react-brand/fonts/fonts.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
